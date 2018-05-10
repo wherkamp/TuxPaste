c32214c5db92815bfd9a7f1aefbabe8e3ce91903
wherkamp <wherkamp@gmail.com>
2018-05-10 15:32:52 -0400

    Update

--- common.php
+++ common.php
@@ -5,7 +5,14 @@
  * Date: 5/9/2018
  * Time: 11:35 AM
  */
+$configJson = json_decode(file_get_contents(ROOT . "/common/config.json"));
+if ($configJson->{'debug'} == "true") {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
 include 'common/config.php';
 $config = new config();
 include 'common/conn.php';
 define('ROOT', $config->baseURl);
+
--- index.php
+++ index.php
@@ -6,18 +6,45 @@
  * Time: 2:22 PM
  */
 include 'common.php';
-if (isset($_GET['paste'])) {
+if ($configJson->{'installed'} == "true") {
+    if (isset($_GET['paste'])) {
 
-} else if (isset($_POST['submit'])) {
+    } else if (isset($_POST['submit'])) {
 
+    } else {
+        if (isset($_GET['post'])) {
+            if ($_GET['post'] == "file") {
+
+                $index = file_get_contents(ROOT . "/public/post-paste-file.html");
+                $index = parseDefault($index);
+                echo $index;
+            } elseif ($_GET['post'] == "text") {
+
+                $index = file_get_contents(ROOT . "/public/post-paste-regular.html");
+                $index = parseDefault($index);
+                echo $index;
+            }
+        } else {
+            $index = file_get_contents(ROOT . "/public/index.html");
+            $index = parseDefault($index);
+            echo $index;
+        }
+    }
 } else {
-    $index = file_get_contents(ROOT . "/public/index.html");
-    $index = parseDefault($index);
-    echo $index;
+    //Do Install
+
+    //Finished!
+    $configJson->{'installed'} = "true";
+    file_put_contents(json_encode($configJson));
+    header("Location: " . ROOT . "/installed");
 }
 function parseDefault(string $content): string
 {
     $content = str_replace("{root}", ROOT, $content);
-
+    if (isset($_GET['installed'])) {
+        $content = str_replace("motd", '<div class="alert alert-success" role="alert">
+        	<strong>Install Successful</strong>
+        </div>', $content);
+    }
     return $content;
 }
